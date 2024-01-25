56e4087525a69ac8150cf3a0e07db7cf865065b2
Jayeon Hwang <jayeon512@gmail.com>
2024-01-25 09:10:25 -0600

    change className and CSS

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,19 +1,26 @@
 export function Display() {
   return (
     <div className="display">
-      <div className="tipAmount">
-        <p>Tip Amont</p>
-        <input type="number" name="tipAmount" />
-        <small>/ per person</small>
-      </div>
+      {/* Tip amount and Total */}
 
-      <div className="totalAmount">
-        <p>Total</p>
-        <input type="number" name="total" />
-        <small>/ per person</small>
+      <div class="col amount-col">
+        <label class="amount-label" for="amount">Tip Amount</label>
+        <label class="sub-label" for="total">/ person</label>
       </div>
+      <div class="col input-col">
+        <p id="amount" class="amount-input">$0.00</p>
+      </div>
+      <div class="col total-col">
+        <label class="total-label" for="total">Total</label>
+        <label class="sub-label" for="total"> / person</label>
+      </div>
+      <div class="col input-col">
+        <p id="total" class="total-input">$0.00</p>
+      </div>
+      <br />
 
-      <button>Reset</button>
+      <label for="reset-btn"></label>
+      <button type="button" class="reset-button" value="Reset">Reset</button>
 
     </div>
   )
--- src/Form.jsx
+++ src/Form.jsx
@@ -1,31 +1,36 @@
 export function Form() {
   return (
-    <div className="form">
-      <p>Bill</p>
-      <input type="number" name="billAmount" />
-
-      <div className="tip %">
-        <p>Select Tip %</p>
-        <input type="radio" id="5%" value={0.05} />
-        <label for="5%">5%</label>
-        <input type="radio" id="10%" value={0.1} />
-        <label for="10%">10%</label>
-        <input type="radio" id="15%" value={0.15} />
-        <label for="15%">15%</label>
-        <input type="radio" id="20%" value={0.2} />
-        <label for="20%">20%</label>
-        <input type="radio" id="25%" value={0.25} />
-        <label for="25%">25%</label>
-        <input type="radio" id="30%" value={0.3} />
-        <label for="30%">30%</label>
-        <input type="number" id="customs" />
-        <label for="customs">customs</label>
+    <form className="input-form">
+      <div className="tip-input">
+        {/* Bill input */}
+        <label for="bill" className="label-title bill">Bill</label>
+        <input type="number" id="bill" className="bill-input" placeholder="0" name="bill" />
+        {/* Tip buttons input */}
+        <div className="tip-btns">
+          <label for="tip" className="label-title select">Select Tip %</label>
+          <div className="col">
+            <button className="tip-buttons" value="5%">5%</button>
+            <button className="tip-buttons" value="10%">10%</button>
+            <button className="tip-buttons" value="15%">15%</button>
+          </div>
+          <div className="col">
+            <button className="tip-buttons" value="25%">25%</button>
+            <button className="tip-buttons" value="50%">50%</button>
+            <input type="number" id="tip" className="custom-input" value="custom" placeholder="Custom" />
+          </div>
+        </div>
       </div>
+      <br />
 
-      <p>Number of People</p>
-      <input type="number" name="numberOfPeople" />
-    </div>
+      {/* Number of people input */}
+      <div className="perPerson-input">
 
+        <label for="quantity" className="label-title num"> Number of People</label>
+
+        <small className="error-message">Can't be zero</small>
+        <input type="number" className="numOfPeople-input" name="numberOfPeople" id="quantity" placeholder="0" />
+      </div>
+    </form>
 
   )
 }
